d3c6335fb163d01bc56a735b7b7b27381a2522ae
Shannon Ormond <sormond@it072691.massey.ac.nz>
2018-05-03 17:00:04 +1200

    Added sample files to directory. Installed matplotlib and pandas packages in script.

--- diffexp.py
+++ diffexp.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
+""" wkdirectory = "/Users/sormond/Desktop//diffexp/DataFiles" # add working directory here
+import os
+os.chdir(wkdirectory) """
+import pandas as pd
+def loadf(file) :
+    df = pd.read_table(file, index_col = 0)
+    return df
+    """ with open(file, 'r') as r:
+        df = r.readlines()
+        return list """
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-1', '--sample1', help = "File of sample 1.")
 parser.add_argument('-2', '--sample2', help = "File of sample 2.")
-parser.add_argument('-o', '--outstem', help = "Stem name for output file. Default stem is 'output' if not given.")
+""" parser.add_argument('-o', '--outstem', help = "Stem name for output file. Default stem is 'output' if not given.") """
 
 args = parser.parse_args()
 
-sample_1 = args.sample1
-sample_2 = args.sample2
+sample1 = loadf(args.sample1)
+sample2 = loadf(args.sample2)
 
-def load(f)
-    with open(f, 'r') as f:
-        fastq_list = f.readlines()
-
+import matplotlib.pyplot as plt
+plt.scatter(sample1[0], sample1[1])
+plt.show()
