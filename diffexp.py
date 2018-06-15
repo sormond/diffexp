e2477b53bc53f6e618d7a5ffed1c8953e580fd1c
Shannon Ormond <sormond@Shannons-MacBook-Pro.local>
2018-06-15 13:32:59 +1200

    Added some exceptions/error/warning messages. Script written to write out data table. Script written to allow choice of plot file format.

--- diffexp.py
+++ diffexp.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-""" import matplotlib.pyplot as plt """
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import numpy.ma as ma # for masking functions
 import scipy
 from scipy.stats import chi2_contingency
 import statsmodels.stats.multitest
-""" from statsmodels.stats import stats
-import stats.multitest """
-
-pd.options.display.max_rows = 999 #delete when finalised
+import sys
 
 def loadf(file) :
+    # loads in expression value files, first row are headers
     df = pd.read_csv(file, sep = "\t")
     global l
     l = df['gene'].tolist()
@@ -21,21 +19,39 @@ def loadf(file) :
     return df
 
 def merge(df1, df2) :
+    # merges two dataframes, if gene row exists in one but not other dataframe, gene will be added with 'NaN' value for sample without gene
     df = df1.join(df2, how='outer')
     return df
 
-# includes both'merge_df' and 'chisqtest' function in one
+# includes both'merge' and 'loadf' function in one
 # function which creates pandas data frame containing list of genes and expression values
 # and adds columns containing calculated chi x-squared values and p-values
+
 def stats_dataframe(file1, file2) :
     # loads in two expression value files and creates dataframes by calling function 'loadf'
-    f1 = loadf(file1) 
-    f2 = loadf(file2)
+    try:
+        f1 = loadf(file1)
+        f2 = loadf(file2)
+    except:
+        print("Error: file loading failed. Ensure two tab-delimited files were entered using appropriate flags. Ensure column headed 'gene' exists containing gene names. Please see README.md for further information on usage.")
+        sys.exit()
     # merges two dataframes into one
+    if len(f1) != len(f2) :
+        print("Warning: Expression value files loaded in do not contain same number of rows.")
     mdf = merge(f1, f2)
+    f1_genes = sorted(f1.index[0:].format())
+    f2_genes = sorted(f2.index[0:].format())
+    if f1_genes != f2_genes :
+        print("Warning: Gene labels are not identical in expression value files loaded.")
+    global o
+    o = list(mdf.columns.values)
     # sums the total of each expession column for statistical calculations
-    A = (sum(mdf['sampleA']))
-    B = (sum(mdf['sampleB']))
+    try:
+        A = (sum(mdf[o[0]]))
+        B = (sum(mdf[o[1]]))
+    except:
+        print("Error: Failed to sum expression values. Please ensure values are numerical only.")
+        sys.exit()
     # creates empty lists for statistical values to be appended to
     xsquared_values = []
     p_values =[]
@@ -50,14 +66,15 @@ def stats_dataframe(file1, file2) :
         if a == 0 and b == 0 :
             xsquared_values.append(np.nan)
             p_values.append(np.nan)
+            print("Warning: Expession values were zero from both samples for gene %s, p-values will be 'NaN' for these and they will not appear on the plot" % (mdf.index[[i]].format()))
         else :
             # calculates x-squared and p-values using chi contigency table and appends values to previously defined lists
             x = np.array([[a,b], [A, B]])
             chi = chi2_contingency(x)
             xsquared_values.append(chi[0])
-            p_values.append(chi[1])
+            p_values.append(chi[1])    
     p_values = np.array(p_values)
-    mask = np.isfinite(p_values)
+    mask = np.isfinite(p_values) 
     pval_corrected = np.empty(p_values.shape)
     null_hypothesis = np.empty(p_values.shape)
     pval_corrected.fill(np.nan)
@@ -73,34 +90,41 @@ def stats_dataframe(file1, file2) :
     mdf['null_hyp_test'] = null_hypothesis.values
     mdf['x_sq_values'] = xsquared_values.values
     mdf['p_values'] = p_values.values
+    mdf.to_csv(args.datafile + ".csv", header=True, sep=',', mode='w')
     return mdf
-        
+
+def makeplot(dataframe) :
+    colors = np.array(df["null_hyp_test"])
+    colors2 = np.empty(colors.shape, dtype=str)
+    for c in range(0, len(colors)) :
+        if colors[c] == True :
+            colors2[c] = 'blue'
+        elif colors[c] == False :
+            colors2[c] = 'red'
+        else :
+            colors2[c] = 'black'
+    df.plot.scatter(o[0], o[1], c=colors2)  # add colour labels
+    if args.outstempdf :
+        plt.savefig(args.outstempdf + ".pdf")
+    elif args.outstemjpeg :
+        plt.savefig(args.outstemjpeg + ".jpeg")
+    else:
+        print("Error: Please specify output plot format and name, with -opdf or -ojpeg followed by stem name of the plot.")
+
+def mainfunction() :
+    global df
+    df = stats_dataframe(args.sample1, args.sample2)
+    makeplot(df)
+
 # command-line executable script
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-1', '--sample1', help = "File of sample 1.")
 parser.add_argument('-2', '--sample2', help = "File of sample 2.")
-""" parser.add_argument('-o', '--outstem', help = "Stem name for output file. Default stem is 'output' if not given.") """
-
+parser.add_argument('-p', '--outstempdf', help = "Stem name for output pdf plot. Must be specified.")
+parser.add_argument('-j', '--outstemjpeg', help = "Stem name for output jpeg plot. Must be specified.")
+parser.add_argument('-d', '--datafile', help = "Stem name for output csv file. Must be specified.")
 args = parser.parse_args()
 
-# calls function and returns to variable 'dataframe'
-df = stats_dataframe(args.sample1, args.sample2)
-df['genes'] = l
-print(df)
-
-""" df = pd.DataFrame(np.random.rand(50, 4), columns=['a', 'b', 'c', 'd']) """
-import matplotlib.pyplot as plt
-""" sampleA = dataframe['sampleA']
-sampleB = dataframe['sampleB']
-print(l)
-ax = plt.scatter(x=l, y=sampleA, color = 'DarkBlue', label = 'Group 1')
-plt.scatter(x=l, y=sampleB, color = 'DarkGreen', label = 'Group 2', ax=ax)
-plt.show() """
-
-df.plot.scatter(x='sampleA', y='sampleB', color='DarkGreen')
-plt.show()
-""" To plot multiple column groups in a single axes, repeat plot method specifying target ax. It is recommended to specify color and label keywords to distinguish each groups.
-In [62]: ax = df.plot.scatter(x='a', y='b', color='DarkBlue', label='Group 1');
-In [63]: df.plot.scatter(x='c', y='d', color='DarkGreen', label='Group 2', ax=ax);
-"""
+# calls function and returns to variable 'df'
+mainfunction()
